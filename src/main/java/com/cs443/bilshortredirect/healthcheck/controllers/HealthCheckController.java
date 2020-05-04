8a947e16a9943cb593ed1b2f4cf3407a78f053be
musabgelisgen <mgelisgen97@gmail.com>
2020-05-04 09:15:12 +0300

    Controllers are converted to RestControllers

--- src/main/java/com/cs443/bilshortredirect/healthcheck/controllers/HealthCheckController.java
+++ src/main/java/com/cs443/bilshortredirect/healthcheck/controllers/HealthCheckController.java
@@ -2,13 +2,13 @@ package com.cs443.bilshortredirect.healthcheck.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@Controller
+@RestController
 public class HealthCheckController {
 
     @GetMapping("/healthCheck")
--- src/main/java/com/cs443/bilshortredirect/link/controllers/RedirectController.java
+++ src/main/java/com/cs443/bilshortredirect/link/controllers/RedirectController.java
@@ -23,7 +23,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
